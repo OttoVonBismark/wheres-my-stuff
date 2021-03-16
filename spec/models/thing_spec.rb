fb45e250d99465baa446992a389283149ff66b13
Aaron Zaborac <aaron.zaborac@outlook.com>
2021-03-16 18:53:14 -0400

    Wires up form/views to display Price.

--- spec/models/thing_spec.rb
+++ spec/models/thing_spec.rb
@@ -5,21 +5,32 @@ require 'rails_helper'
 RSpec.describe Thing, type: :model do
   let(:valid_attributes) {
     {
-      name: 'Some Thing'
+      name: 'Some Thing',
+      price: 1.00
     }
   }
 
   describe 'Creating a Thing' do
     context 'with valid attributes' do
-      it 'creates a Thing' do
+      it 'creates a Thing with a name and a price greater than zero' do
         thing = Thing.new(valid_attributes)
+        expect(thing).to be_valid
+      end
+
+      it 'creates a Thing with a price of zero' do
+        thing = Thing.new(name: 'Some thing', price: 0.00)
         expect(thing).to be_valid
       end
     end
 
     context 'without valid attributes' do
-      it 'rejects the Thing' do
+      it 'rejects the Thing without a name' do
         thing = Thing.new
+        expect(thing).to_not be_valid
+      end
+
+      it 'rejects a price less than zero' do
+        thing = Thing.new(name: 'Some Thing', price: -1.00)
         expect(thing).to_not be_valid
       end
     end
