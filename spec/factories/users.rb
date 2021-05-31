79ac7d0c0602a49e262517604a456327b33f869c
Aaron Zaborac <aaron.zaborac@outlook.com>
2021-05-31 11:52:33 -0400

    Appease rubocop. Fix RSpec.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,19 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user do
-    
+    username { Faker::Internet.username }
+    email { Faker::Internet.email }
+    password { 'foobarbaz' }
+    password_confirmation { password }
+    confirmed_at { DateTime.yesterday }
+
+    trait :unconfirmed do
+      confirmed_at { nil }
+    end
+
+    trait :without_password do
+      password { nil }
+    end
   end
 end
